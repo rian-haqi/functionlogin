6851d2e869014c4a76e86d70a156ce4fd495404d
rian-haqi <141999480+rian-haqi@users.noreply.github.com>
2023-10-04 14:53:18 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function login(username , pass){
+  console.log("=========== ") 
+  console.log("login")
+  console.log("=========== ")
+  console.log("username :" +(username))
+  console.log("pass :" + (pass))
+  console.log("========= ") 
+  
+}
+
+login("Rian haqi n","1234567890")
